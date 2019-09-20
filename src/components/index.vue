59acbb5d98d8512456e9321e1e2e04b077034377
caohongqing <caohongqing@weyao.com>
2019-09-20 10:58:08 +0800

    vueslidershow( 自己封装一个vue的响应式自适应轮播图组件 )

---
+++ src/components/index.vue
@@ -0,0 +1,63 @@
+<template>
+   <div>
+     <slider-show :slides="slides" :inv="invTime"></slider-show>
+   </div>
+</template>
+
+<script>
+import sliderShow from './sliderShow'
+export default {
+components: {
+    sliderShow
+},
+  data () {
+    return {
+      invTime: 2000,
+      slides: [
+        {
+          src: require('../assets/1.jpg'),
+          title: '测试测试测试1',
+          href: 'detail/analysis'
+        },
+        {
+          src: require('../assets/2.jpg'),
+          title: '测试测试测试2',
+          href: 'detail/count'
+        },
+        {
+          src: require('../assets/1.jpg'),
+          title: '测试测试测试1',
+          href: 'detail/analysis'
+        },
+        {
+          src: require('../assets/2.jpg'),
+          title: '测试测试测试2',
+          href: 'detail/count'
+        },
+        {
+          src: require('../assets/1.jpg'),
+          title: '测试测试测试1',
+          href: 'detail/analysis'
+        },
+        {
+          src: require('../assets/2.jpg'),
+          title: '测试测试测试2',
+          href: 'detail/count'
+        }
+      ],
+    }
+  },
+  created: function () {
+	    this.$http.get('api/slidesList')
+	    .then((res) => {
+	      this.slides = res.data
+	    }, (err) => {
+	      console.log(err)
+	    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

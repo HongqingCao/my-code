41317f125214c95e2df4e891921e9aa1fddc17c2
CAOHONGQING\Administrator <caocoder@gmail.com>
2019-10-25 11:12:37 +0800

    微信国庆头像生成工具

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100%;
+  width: 100%;
+  user-select: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
